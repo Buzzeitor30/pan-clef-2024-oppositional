afb0f3a8ac5a1a7d88fe975dc01e458cb2e216a2
Elias Urios Alacreu <eliasurios3001@gmail.com>
2024-05-13 23:31:25 +0200

    model

---
+++ classif_experim/model.py
@@ -0,0 +1,158 @@
+from dataclasses import dataclass
+from typing import Optional
+from torch import nn
+import torch
+from transformers.pipelines import Pipeline
+
+
+from transformers.data.data_collator import (
+    DataCollatorWithPadding,
+    pad_without_fast_tokenizer_warning,
+)
+from transformers.models.roberta import RobertaForSequenceClassification
+from transformers.modeling_outputs import SequenceClassifierOutput
+from transformers.pipelines.text_classification import TextClassificationPipeline
+
+
+
+class RobertaClassificationHeadExtended(nn.Module):
+    """Head for sentence-level classification tasks."""
+
+    def __init__(self, config):
+        super(RobertaClassificationHeadExtended, self).__init__()
+        self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+        classifier_dropout = (
+            config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
+        )
+        self.dropout = nn.Dropout(classifier_dropout)
+        self.out_proj = nn.Linear(config.hidden_size + 4, config.num_labels)
+
+    def forward(self, features, **kwargs):
+        x = features[:, 0, :]  # take <s> token (equiv. to [CLS])
+        x = self.dropout(x)
+        x = self.dense(x)
+        x = torch.tanh(x)
+        x = self.dropout(x)
+        x = torch.cat((x,kwargs["emotions"]), dim=1)
+        x = self.out_proj(x)
+        return x 
+# **{inputs}
+class BertForSequenceClassificationExtended(RobertaForSequenceClassification):
+    def __init__(self, config):
+        super(BertForSequenceClassificationExtended, self).__init__(config)
+        self.classifier = RobertaClassificationHeadExtended(config)
+
+    def forward(
+        self,
+        input_ids: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        token_type_ids: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.Tensor] = None,
+        head_mask: Optional[torch.Tensor] = None,
+        inputs_embeds: Optional[torch.Tensor] = None,
+        labels: Optional[torch.Tensor] = None,
+        output_attentions: Optional[bool] = None,
+        output_hidden_states: Optional[bool] = None,
+        return_dict: Optional[bool] = None,
+        emotions: Optional[torch.Tensor] = None,
+    ):
+
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+        outputs = self.roberta(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
+        )
+        sequence_output = outputs[0]
+        logits = self.classifier(sequence_output, **{"emotions":emotions})
+
+        loss = None
+        if labels is not None:
+            if self.config.problem_type is None:
+                if self.num_labels == 1:
+                    self.config.problem_type = "regression"
+                elif self.num_labels > 1 and (
+                    labels.dtype == torch.long or labels.dtype == torch.int
+                ):
+                    self.config.problem_type = "single_label_classification"
+                else:
+                    self.config.problem_type = "multi_label_classification"
+
+            if self.config.problem_type == "regression":
+                loss_fct = nn.MSELoss()
+                if self.num_labels == 1:
+                    loss = loss_fct(logits.squeeze(), labels.squeeze())
+                else:
+                    loss = loss_fct(logits, labels)
+            elif self.config.problem_type == "single_label_classification":
+                loss_fct = nn.CrossEntropyLoss()
+                loss = loss_fct(logits.view(-1, self.num_labels), labels.view(-1))
+            elif self.config.problem_type == "multi_label_classification":
+                loss_fct = nn.BCEWithLogitsLoss()
+                loss = loss_fct(logits, labels)
+        if not return_dict:
+            output = (logits,) + outputs[2:]
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
+
+
+@dataclass
+class CustomDataCollator(DataCollatorWithPadding):
+    def __call__(self, features):
+        batch = {}
+        batch["input_ids"] = torch.tensor([feat["input_ids"] for feat in features])
+        batch["attention_mask"] = torch.tensor([feat["attention_mask"] for feat in features])
+        batch["labels"] = torch.tensor([feat["label"] for feat in features])
+        batch["emotions"] = torch.tensor([feature["emotions"] for feature in features])
+        return batch
+
+
+
+class PipelineCustom(Pipeline):
+    def _sanitize_parameters(self, return_all_scores=None, function_to_apply=None, top_k="", **tokenizer_kwargs):
+        # Using "" as default argument because we're going to use `top_k=None` in user code to declare
+        # "No top_k"
+        preprocess_params = tokenizer_kwargs
+
+        postprocess_params = {}
+        if hasattr(self.model.config, "return_all_scores") and return_all_scores is None:
+            return_all_scores = self.model.config.return_all_scores
+
+        if isinstance(top_k, int) or top_k is None:
+            postprocess_params["top_k"] = top_k
+            postprocess_params["_legacy"] = False
+        elif return_all_scores is not None:
+            if return_all_scores:
+                postprocess_params["top_k"] = None
+            else:
+                postprocess_params["top_k"] = 1
+
+
+        if function_to_apply is not None:
+            postprocess_params["function_to_apply"] = function_to_apply
+        return preprocess_params, {}, postprocess_params
+
+    def preprocess(self, inputs):
+        text_input = self.tokenizer(inputs["text"], padding=True, truncation= True, max_length=256, return_tensors="pt")
+        emotions = torch.Tensor(inputs["emotions"])
+        return {"emotions": emotions, **text_input}
+
+    def _forward(self, model_inputs):
+        outputs = self.model(**model_inputs)
+        return outputs
+
+    def postprocess(self, model_outputs):
+        best_class = model_outputs["logits"].softmax(-1)
+        return best_class
