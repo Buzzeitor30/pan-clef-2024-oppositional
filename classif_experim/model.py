b9b6dcaf4b6481529a44ba0a80505ed4b9a05072
Elias Urios Alacreu <eliasurios3001@gmail.com>
2024-05-22 14:23:42 +0200

    more results

--- classif_experim/model.py
+++ classif_experim/model.py
@@ -25,11 +25,11 @@ class RobertaClassificationHeadExtended(nn.Module):
             config.classifier_dropout if config.classifier_dropout is not None else config.hidden_dropout_prob
         )
         self.dropout = nn.Dropout(classifier_dropout)
-        self.out_proj = nn.Linear(config.hidden_size, config.num_labels)
+        self.out_proj = nn.Linear(config.hidden_size + 4, config.num_labels)
 
     def forward(self, features, **kwargs):
         x = features[:, 0, :]  # take <s> token (equiv. to [CLS])
-        #x = torch.cat((x,kwargs["emotions"]), dim=1)
+        x = torch.cat((x,kwargs["emotions"]), dim=1)
         x = self.out_proj(x)
         return x 
 # **{inputs}
